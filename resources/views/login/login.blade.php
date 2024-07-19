b6a63e72e81fce3db2fc93287c39613fd3df5857
thanh157 <hd.thanh157@gmail.com>
2024-07-19 11:27:15 +0700

    feat: [SSP-7] add UI login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -22,13 +22,12 @@
                 <!-- header -->
                 <div class="login-header">
                     <h1>Chào mừng đến bạn với CÓINCIDENCE <i class="fa-solid fa-check"></i></h1>
-                    <p>Vui lòng đăng nhập vào tài khoản của bạn</p>
                 </div>
                 <!-- form  -->
                 <form class="login-form">
-                    <label for="email">Nhập vào email hoặc số điện thoại</label>
+                    <label for="email">Tài khoản/Email</label>
                     <input type="email" id="email" placeholder="Email" required>
-                    <label for="password">Nhập mật khẩu</label>
+                    <label for="password">Mật khẩu:</label>
                     <input type="password" id="password" placeholder="Mật khẩu" required>
                     <div class="login-options">
                         <label class="remember-me">
@@ -39,17 +38,6 @@
                     </div>
                     <button type="submit">Đăng nhập</button>
                 </form>
-                <!-- footer -->
-                <div class="login-footer">
-                    <p class="login">Bạn mới sử dụng nền tảng của chúng tôi? <a href="#">Tạo tài khoản mới</a></p>
-                    <p class="footer">hoặc</p>
-                    <div class="social-login">
-                        <a class="item-fb" href="#"><i class="fa-brands fa-facebook"></i></a>
-                        <a class="item-tw" href="#"><i class="fa-brands fa-twitter"></i></a>
-                        <a class="item-insta" href="#"><i class="fa-brands fa-instagram"></i></a>
-                        <a class="item-gg" href="#"><i class="fa-brands fa-google"></i></a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>

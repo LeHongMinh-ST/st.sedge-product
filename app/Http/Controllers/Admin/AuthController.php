ed4da123ff05e0b59a27def0f73ce15002778262
Nguyễn Phương Nam <6660256@sv.vnua.edu.vn>
2024-08-03 17:15:37 +0700

    refactor: fix login

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\login\LoginRequest;
+use App\Http\Requests\Login\LoginRequest;
 
 class AuthController extends Controller
 {

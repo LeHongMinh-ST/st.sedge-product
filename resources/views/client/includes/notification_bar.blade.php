ea6e1a5d3a0056bb4d9fd6d6b6ad41c8f8cb4d1c
Trương Tuấn Hoàng <6662012@sv.vnua.edu.vn>
2024-08-03 16:46:39 +0700

    refactor: [SSP-21] fix link button

--- resources/views/client/includes/notification_bar.blade.php
+++ resources/views/client/includes/notification_bar.blade.php
@@ -10,24 +10,6 @@
                             </div>
                         </div>
                     </li>
-                    <li class="noti-wrap noti-text-wrap">
-                        <div class="noti-text">
-                            <p>FreeShip từ ngày 01 đến 10 tháng 7</p>
-                        </div>
-                    </li>
-                    <li class="noti-wrap noti-icon-wrap">
-                        <div class="noti-icon-block">
-                            <ul class="shop-element">
-                                <li class="side-wrap search-wrap">
-                                    <div class="search-wrapper">
-                                        <a href="#searchmodal" data-bs-toggle="modal">
-                                            <span class="search-icon"><svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg> Tìm kiếm</span>
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
                 </ul>
             </div>
         </div>

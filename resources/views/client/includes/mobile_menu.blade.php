13d2e22a2af3f395ac573bbafe3e1d8e6fa9cd74
Trương Tuấn Hoàng <6662012@sv.vnua.edu.vn>
2024-09-19 16:09:47 +0700

    refactor: [SSP-30] delete contact in mobile menu

--- resources/views/client/includes/mobile_menu.blade.php
+++ resources/views/client/includes/mobile_menu.blade.php
@@ -35,11 +35,6 @@
                         </a>
                     </li>
                     <li class="menu-link">
-                        <a href="{{route('todo.contact')}}" class="link-title" >
-                            <span class="sp-link-title">Liên hệ</span>
-                        </a>
-                    </li>
-                    <li class="menu-link">
                         <a href="{{route('todo.checkorder')}}" class="link-title" >
                             <span class="sp-link-title">Kiểm tra đơn hàng</span>
                         </a>
